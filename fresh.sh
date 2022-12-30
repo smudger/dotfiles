b6a2b41032221d99b5516e89f6361b53cc1afeb3
Louis Smith <15144861+smudger@users.noreply.github.com>
2022-12-30 22:10:43 +0000

    Replace hardcoding of install location

--- fresh.sh
+++ fresh.sh
@@ -37,9 +37,12 @@ $HOME/.composer/vendor/bin/valet install
 # Create a Code directory
 mkdir $HOME/Code
 
+# Get the absolute path to this repo
+DOTFILES=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -s $DOTFILES/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
