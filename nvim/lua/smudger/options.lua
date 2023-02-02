7ab3093a9f3d3f0daa960fffe4b27c4d2ffbf143
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-02-02 20:57:16 +0000

    Add Neovim plugins

--- nvim/lua/smudger/options.lua
+++ nvim/lua/smudger/options.lua
@@ -27,4 +27,4 @@ vim.opt.backup = true -- automatically save a backup file
 vim.opt.backupdir:remove('.') -- keep backups out of the current directory
 vim.opt.shortmess:append({ I = true }) -- disable the splash screen
 vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
-vim.opt.signcolumn = 'yes:1' -- leave space for git signs column even if there aren't any
+vim.opt.signcolumn = 'yes:2' -- leave space for git signs and LSP columns even if there aren't any
