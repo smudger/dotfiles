3b5da1d76b6ede5292f8f5eec1b53c0394b831ef
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-02-06 18:41:16 +0000

    Add code completion

--- nvim/lua/smudger/options.lua
+++ nvim/lua/smudger/options.lua
@@ -27,4 +27,5 @@ vim.opt.backup = true -- automatically save a backup file
 vim.opt.backupdir:remove('.') -- keep backups out of the current directory
 vim.opt.shortmess:append({ I = true }) -- disable the splash screen
 vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+vim.opt.completeopt = 'menuone,longest,preview' -- settings for code completion popup
 vim.opt.signcolumn = 'yes:2' -- leave space for git signs and LSP columns even if there aren't any
